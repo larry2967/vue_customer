d92b9671a2a9cf81f77f49c713847375ba7ecc48
larry2967 <xx01478xx@gmail.com>
2021-11-26 14:10:03 +0800

    version-1(only connect to api)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
+// import './plugins/bootstrap-vue'
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-createApp(App).mount('#app')
+
+createApp(App)
+  .use(router)
+  // .use(BootstrapVue)
+  // .use(IconsPlugin)
+  .mount('#app')
