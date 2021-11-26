a30d3edcd5322b9d8a9af05f4da7dbbe39447350
larry2967 <xx01478xx@gmail.com>
2021-11-26 14:11:19 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
-// import './plugins/bootstrap-vue'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 
 
 createApp(App)
   .use(router)
-  // .use(BootstrapVue)
-  // .use(IconsPlugin)
+
   .mount('#app')
